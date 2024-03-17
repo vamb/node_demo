f996b78323b4da0fc73d3564d4092778eb87800f
SUYANG\Admin <vambknight@gmail.com>
2024-03-17 10:41:24 +0800

    feat: code update

---
+++ demo9Express/README.md
@@ -0,0 +1,6 @@
+## 什么是 Express
+#### express 是基于 Node.js 平台，快速，开放，极简的 web 框架
+#### express 的作用和 Node.js 内置的 http 模块类似，是专门用来创建 Web 服务器的
+#### express 的本质，就是一个 npm 上的第三方包，提供了快速创建 Web 服务器的便捷方式
+
+### 使用 Express 可以方便的快速的创建 web 网站 或者 API 接口服务器
---
+++ demo9Express/demo1Express.js
@@ -0,0 +1,23 @@
+const express = require('express')
+// 创建 web 服务器
+const app = express()
+
+app.get('/user/:id', (req, res)=>{
+  console.log('params query', req.params, req.query)
+  res.send(JSON.stringify(req.params) + ' ' + JSON.stringify(req.query))
+})
+
+app.get('/user', (req, res)=>{
+  res.send({name: 'testName', age: '25', gender: 'male'})
+})
+
+
+app.post('/user', (req, res)=>{
+  console.log('query', req?.query)
+  res.send('请求成功')
+})
+
+// 调用 listen 方法并指定端口号来启动服务
+app.listen(8356, ()=>{
+  console.log('express server running at http://127.0.0.1:8356')
+})
